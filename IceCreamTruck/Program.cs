a2f936bad5b7c04d5a8b0b7060e3e26a059ca900
Arkael <boltthedog27@gmail.com>
2022-07-27 12:04:16 +0300

    Added Trucks, icecream, outlining story

---
+++ IceCreamTruck/Program.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IceCreamTruck
+{
+    internal class Program
+    {
+        static void NextDialogue() {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("\t ->");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static void Main(string[] args)
+        {
+            IceCreamTruck truck1 = new IceCreamTruck();
+            Random rand = new Random();
+            double money = rand.Next(10);
+            money /= 10;
+            money += rand.Next(100) + 150;
+            double happiness = 10;
+            string flavour;
+            string favflavour = "Vanilla";
+
+            switch (rand.Next(10))
+            {
+                case 0:
+                    favflavour = "Strawberry";
+                    break;
+                case 1:
+                    favflavour = "Chocolate";
+                    break;
+                case 2:
+                    favflavour = "Pistachio";
+                    break;
+                case 3:
+                    favflavour = "Blueberry";
+                    break;
+                case 4:
+                    favflavour = "Mint";
+                    break;
+                case 5:
+                    favflavour = "Cherry";
+                    break;
+                case 6:
+                    favflavour = "Mango";
+                    break;
+                case 7:
+                    favflavour = "Lavender";
+                    break;
+                case 8:
+                    favflavour = "Lemon";
+                    break;
+                case 9:
+                    favflavour = "Coffee";
+                    break;
+            }
+            int lastdays = rand.Next(20);
+            Console.WriteLine("You really want some icecream.");
+            NextDialogue();
+            Console.WriteLine("You REALLY want some icecream.");
+            NextDialogue();
+            Console.WriteLine("It's been...");
+            NextDialogue();
+            Console.WriteLine("It's been {0} days since the last time you ate icecream...", lastdays);
+            NextDialogue();
+            Console.WriteLine("{0} days?!", lastdays);
+            NextDialogue();
+            Console.WriteLine("You have to get some icecream!");
+            NextDialogue();
+            Console.WriteLine("You don't want to imagine going a single more day without icecream!");
+            NextDialogue();
+            Console.WriteLine("There are 5 icecream trucks in the town.");
+            NextDialogue();
+            Console.WriteLine("You have saved up {0} cash, that should be enough to get some!", money);
+            NextDialogue();
+            Console.WriteLine("Go out there and get yourself some icecream!");
+            NextDialogue();
+            Console.WriteLine("Remember, your favourite flavour is {0}.", favflavour);
+            NextDialogue();
+            Console.WriteLine("Now, go get that icecream!");
+            NextDialogue();
+
+
+        Reset:
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\t \t Happiness meter:");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("*--------------------------------------------------*");
+            Console.Write("|");
+            Console.ForegroundColor = ConsoleColor.Red;
+            if (happiness >= 200) { Console.ForegroundColor = ConsoleColor.Magenta; }
+            if (happiness >= 400) { Console.ForegroundColor = ConsoleColor.DarkYellow; }
+            if (happiness >= 600) { Console.ForegroundColor = ConsoleColor.Yellow; }
+            if (happiness >= 800) { Console.ForegroundColor = ConsoleColor.Green; }
+            if (happiness >= 1000) { Console.ForegroundColor = ConsoleColor.Blue; }
+            for (int i = 0; i < 50; i++)
+            {
+                if (happiness / 20 >= i)
+                {
+                    Console.Write("=");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("|");
+            Console.WriteLine("*--------------------------------------------------*");
+            Console.WriteLine("My cash: {0}", money);
+            Console.WriteLine();
+            truck1.ShowMenu();
+            Console.WriteLine("Which one do I buy?");
+            flavour = truck1.BuyIceCream(Console.ReadLine(), ref money, ref happiness);
+            if (flavour == favflavour)
+            {
+                happiness += 15;
+            }
+            NextDialogue();
+            goto Reset;
+
+
+        }
+
+
+        class IceCreamTruck
+        { 
+            Random rand = new Random();
+            IceCream[] inventory = new IceCream[5];
+            int[] invAmount = new int[5];
+            double[] prices = new double[5];
+
+            double PriceInitialyser()
+            {
+                double result = rand.Next(10);
+                result /= 10;
+                result += rand.Next(20) + 10;
+                return result;
+            }
+
+            public IceCreamTruck()
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if (i > 0)
+                    {
+                        inventory[i] = new IceCream();
+                    }
+                    else
+                    {
+                        inventory[i] = new IceCream("Vanilla");
+                    }
+                    invAmount[i] = rand.Next(5);
+                    prices[i] = PriceInitialyser();
+                }
+            }
+
+            public void ShowMenu()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\t \t MENU:");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                for (int i = 0; i < inventory.Length; i++)
+                {
+                    this.FlovourColour(inventory[i].Flavour);
+                    Console.Write(inventory[i].Flavour);
+                    Console.ForegroundColor= ConsoleColor.White;
+                    for (int j = 0; j < 11 - inventory[i].Flavour.Length; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write("- ");
+                    Console.WriteLine(prices[i]);
+                }
+                Console.WriteLine();
+            }
+
+            void FlovourColour(string flavour)
+            {
+                switch (flavour)
+                {
+                    case "Vanilla":
+                        Console.ForegroundColor= ConsoleColor.White;
+                        break;
+                    case "Strawberry":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    case "Chocolate":
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        break;
+                    case "Pistachio":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case "Blueberry":
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case "Mint":
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        break;
+                    case "Cherry":
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        break;
+                    case "Mango":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case "Lavender":
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case "Lemon":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case "Coffee":
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        break;
+                }
+
+
+                
+            }
+
+            public string BuyIceCream(string flavour, ref double money, ref double happiness)
+            {
+                string result = "null";
+                int choice = 0;
+                for (int i = 0; i < 5 && choice == 0; i++)
+                {
+                    if (inventory[i].Flavour == flavour)
+                    {
+                        choice = i + 1;
+                    }   
+                }
+
+                if (choice == 0)
+                {
+                    Console.WriteLine("Sorry, I couldn't hear you quite right. Which flavour was that?");
+                }
+                else if (money >= prices[choice-1] && invAmount[choice-1] > 0)
+                {
+                    invAmount[choice-1]--;
+                    result = inventory[choice - 1].Flavour;
+                    Console.WriteLine("There you go! One {0} icecream for you! It'll be {1}.", inventory[choice-1].Flavour, prices[choice-1]);
+                    happiness += inventory[choice - 1].Quality;
+                    money -= prices[choice - 1];
+                } else if (invAmount[choice - 1] > 0)
+                {
+                    Console.WriteLine("Sorry, kiddo, I can't do that one. You're {0} short.", prices[choice-1] - money);
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, kiddo, I can't do that one. I'm afraid we're out of stock!");
+                }
+
+                return result;
+            }
+
+
+        }
+
+
+        class IceCream
+        {
+            string flavour;
+            int quality;
+            static Random rand = new Random();
+
+
+            public IceCream()
+            {
+                quality = rand.Next(9);
+
+                switch (quality)
+                {
+                    case 0:
+                        flavour = "Strawberry";
+                        break;
+                    case 1:
+                        flavour = "Chocolate";
+                        break;
+                    case 2:
+                        flavour = "Pistachio";
+                        break;
+                    case 3:
+                        flavour = "Blueberry";
+                        break;
+                    case 4:
+                        flavour = "Mint";
+                        break;
+                    case 5:
+                        flavour = "Cherry";
+                        break;
+                    case 6:
+                        flavour = "Mango";
+                        break;
+                    case 7:
+                        flavour = "Lavender";
+                        break;
+                    case 8:
+                        flavour = "Lemon";
+                        break;
+                    case 9:
+                        flavour = "Coffee";
+                        break;
+                }
+                quality = 50 + rand.Next(50);
+            }
+
+            public IceCream(string flavor)
+            {
+                flavour = flavor;
+                quality = 60 + rand.Next(40);
+            }
+
+            public string Flavour
+            {
+                get { return flavour; }
+            }
+
+            public int Quality
+            {
+                get { return quality + rand.Next(10) - rand.Next(10); }
+            }
+
+        }
+
+    }
+}
